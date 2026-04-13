3de27d99018c349e3b58bf6949c23fc07bec624d
HarshDev1809 <harsh.dev182000@gmail.com>
2026-04-13 15:52:43 +0000

    changed esc to jk

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.shiftwidth = 8 -- Size of an indent
+opt.tabstop = 8 -- Number of spaces tabs count for
+opt.softtabstop = 8 -- Number of spaces tabs count for while editing
+opt.expandtab = true -- Use spaces instead of tabs
+
+vim.opt.clipboard = "unnamedplus"
